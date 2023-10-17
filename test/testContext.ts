842c121a14c8e73026b034adddc2c1e7b1e41fe1
Igor Savin <iselwin@gmail.com>
2023-10-17 11:11:50 +0300

    Compile test context in CI (#24)

---
+++ test/testContext.ts
@@ -0,0 +1,34 @@
+import type { AwilixContainer } from 'awilix'
+import { asClass, Lifetime } from 'awilix'
+import type { Resolver } from 'awilix/lib/resolvers'
+
+export const SINGLETON_CONFIG = { lifetime: Lifetime.SINGLETON }
+
+export type DependencyOverrides = Partial<DiConfig>
+class SomeModule {}
+
+export function registerDependencies(
+  diContainer: AwilixContainer,
+  dependencyOverrides: DependencyOverrides = {},
+): void {
+  const diConfig: DiConfig = {
+    module: asClass(SomeModule, {
+      ...SINGLETON_CONFIG,
+      asyncInit: 'start',
+      asyncDispose: 'close',
+      asyncDisposePriority: 10,
+      enabled: true,
+    }),
+  }
+  diContainer.register(diConfig)
+
+  for (const [dependencyKey, dependencyValue] of Object.entries(dependencyOverrides)) {
+    diContainer.register(dependencyKey, dependencyValue)
+  }
+}
+
+type DiConfig = Record<keyof Dependencies, Resolver<any>>
+
+export interface Dependencies {
+  module: SomeModule
+}
